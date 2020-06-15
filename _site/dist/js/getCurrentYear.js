ea3255ce16e44cedcbe6c588cd156af02cdf44a4
Juan Martín García <hello@juangarcia.com.ar>
2020-06-15 10:06:11 -0300

    Get the year dynamically

---
+++ _site/dist/js/getCurrentYear.js
@@ -0,0 +1,4 @@
+"use strict";
+
+const yearContainer = document.querySelector('footer ul li h4 span');
+yearContainer.innerHTML = new Date().getFullYear();
---
+++ assets/js/getCurrentYear.js
@@ -0,0 +1,3 @@
+const yearContainer = document.querySelector('footer ul li h4 span');
+
+yearContainer.innerHTML = new Date().getFullYear();
