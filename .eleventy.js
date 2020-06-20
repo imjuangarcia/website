b59f5329621a7c5763ffa098af493e9e3b73abbf
Juan Martín García <hello@juangarcia.com.ar>
2020-06-20 10:45:58 -0300

    Refactor the order of the files

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,13 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./assets/");
+module.exports = config => {
+  config.addPassthroughCopy('./src/fonts/');
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    
+    dir: {
+      input: 'src',
+      output: 'dist'
+    }
+  };
 };
--- _site/dist/js/getCurrentYear.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-const yearContainer = document.querySelector('footer ul li h6 span');
-yearContainer.innerHTML = new Date().getFullYear();
