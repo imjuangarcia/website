866ca13a88ef5fdf17de690b1cc31c72e59f9569
Juan Martín García <hello@juangarcia.com.ar>
2021-03-06 17:35:01 -0300

    Move critical pages styles to subdirectory

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -33,7 +33,7 @@ const calculateOutput = ({ history }) => {
 // The main Sass method grabs all root Sass files,
 // processes them, then sends them to the output calculator
 const sass = () => {
-  return src('./src/styles/*.scss')
+  return src('./src/styles/pages/*.scss')
     .pipe(sassProcessor().on('error', sassProcessor.logError))
     .pipe(
       cleanCSS(
